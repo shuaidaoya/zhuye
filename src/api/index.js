82eb53d1b80269556123401a2b3ef1729977cc90
帅帅的小站 <shuaidaoya@gmail.com>
2025-03-14 14:15:40 +0800

    更新 index.js

--- src/api/index.js
+++ src/api/index.js
@@ -7,36 +7,36 @@ import fetchJsonp from "fetch-jsonp";
 
 // 获取音乐播放列表
 export const getPlayerList = async (server, type, id) => {
-  const res = await fetch(
-    `${import.meta.env.VITE_SONG_API}?server=${server}&type=${type}&id=${id}`,
-  );
-  const data = await res.json();
+  const res = await fetch(
+    `${import.meta.env.VITE_SONG_API}?server=${server}&type=${type}&id=${id}`,
+  );
+  const data = await res.json();
 
-  if (data[0].url.startsWith("@")) {
-    // eslint-disable-next-line no-unused-vars
-    const [handle, jsonpCallback, jsonpCallbackFunction, url] = data[0].url.split("@").slice(1);
-    const jsonpData = await fetchJsonp(url).then((res) => res.json());
-    const domain = (
-      jsonpData.req_0.data.sip.find((i) => !i.startsWith("http://ws")) ||
-      jsonpData.req_0.data.sip[0]
-    ).replace("http://", "https://");
+  if (data[0].url.startsWith("@")) {
+    // eslint-disable-next-line no-unused-vars
+    const [handle, jsonpCallback, jsonpCallbackFunction, url] = data[0].url.split("@").slice(1);
+    const jsonpData = await fetchJsonp(url).then((res) => res.json());
+    const domain = (
+      jsonpData.req_0.data.sip.find((i) => !i.startsWith("http://ws")) ||
+      jsonpData.req_0.data.sip[0]
+    ).replace("http://", "https://");
 
-    return data.map((v, i) => ({
-      name: v.name || v.title,
-      artist: v.artist || v.author,
-      url: domain + jsonpData.req_0.data.midurlinfo[i].purl,
-      cover: v.cover || v.pic,
-      lrc: v.lrc,
-    }));
-  } else {
-    return data.map((v) => ({
-      name: v.name || v.title,
-      artist: v.artist || v.author,
-      url: v.url,
-      cover: v.cover || v.pic,
-      lrc: v.lrc,
-    }));
-  }
+    return data.map((v, i) => ({
+      name: v.name || v.title,
+      artist: v.artist || v.author,
+      url: domain + jsonpData.req_0.data.midurlinfo[i].purl,
+      cover: v.cover || v.pic,
+      lrc: v.lrc,
+    }));
+  } else {
+    return data.map((v) => ({
+      name: v.name || v.title,
+      artist: v.artist || v.author,
+      url: v.url,
+      cover: v.cover || v.pic,
+      lrc: v.lrc,
+    }));
+  }
 };
 
 /**
@@ -45,41 +45,43 @@ export const getPlayerList = async (server, type, id) => {
 
 // 获取一言数据
 export const getHitokoto = async () => {
-  const res = await fetch("https://v1.hitokoto.cn");
-  return await res.json();
+  const res = await fetch("https://v1.hitokoto.cn");
+  return await res.json();
 };
 
 /**
  * 天气
  */
-
+/*
 // 获取高德地理位置信息
 export const getAdcode = async (key) => {
-  const res = await fetch(`https://restapi.amap.com/v3/ip?key=${key}`);
-  return await res.json();
+  const res = await fetch(`https://restapi.amap.com/v3/ip?key=${key}`);
+  return await res.json();
 };
 
 // 获取高德地理天气信息
-export const getAmapWeather = async (key, city) => {
-  const res = await fetch(
-    `https://restapi.amap.com/v3/weather/weatherInfo?key=${key}&city=${city}`,
-  );
-  return await res.json();
+export const getWeather = async (key, city) => {
+  const res = await fetch(
+    `https://restapi.amap.com/v3/weather/weatherInfo?key=${key}&city=${city}`,
+  );
+  return await res.json();
 };
-
+*/
 // 获取韩小韩天气 API
-export const getVvhanWeather = async () => {
-  const url = 'https://api.vvhan.com/api/weather';
-  try {
-    const res = await fetch(url, {
-      mode: 'cors' // 新增跨域模式
-    });
-    if (!res.ok) throw new Error(`HTTP错误! 状态码: ${res.status}`);
-    const data = await res.json();
-    console.log('韩小韩API返回数据:', data); // 新增调试日志
-    return data;
-  } catch (error) {
-    console.error('请求天气数据时出错:', error);
-    return null;
-  }
+// https://api.vvhan.com/api/weather
+export const getWeather = async () => {
+  const url = 'https://api.vvhan.com/api/weather';
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    if (data.success) {
+      return data;
+    } else {
+      console.error('获取天气数据失败:', data.message);
+      return null;
+    }
+  } catch (error) {
+    console.error('请求天气数据时出错:', error);
+    return null;
+  }
 };
